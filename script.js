1f447322e870111bcb964acf77fecc4cb75a46f8
sydmdry <sydneymariedryden@gmail.com>
2020-09-07 19:52:30 -0600

    news api almost working

--- script.js
+++ script.js
@@ -3,6 +3,10 @@
 
 const petitionsURL='https://api.whitehouse.gov/v1/petitions.json?';
 
+const newsURL='https://newsapi.org/v2/top-headlines?'
+
+const newsApiKey='37d1fa5af2a84f389a019d1f80faf045'
+
 
 
 function formatQueryParams(params){
@@ -38,6 +42,27 @@ function getPetitions(search){
 }
 
 
+function getNews(search){
+    const params={
+        apiKey: newsApiKey,
+        q: search,
+        pageSize: 2
+    }
+    const queryString = formatQueryParams(params)
+    const fullNewsURL = newsURL + queryString;
+    console.log(fullNewsURL)
+
+    fetch(fullNewsURL)
+    .then(response=>{
+        if(response.ok){
+            return response.json()
+        }
+        throw new Error(response.statusText);
+    })
+    .then(responseJson => console.log(responseJson))
+}
+
+
 
     function displayPetitions(responseJson){
         console.log(responseJson);
@@ -45,7 +70,7 @@ function getPetitions(search){
         for(let i=0; i<responseJson.results.length; i++){
         $('#js-petitons-list').append(
             `<li><h3>${responseJson.results[i].title}</h3>
-            <a href='${responseJson.results[i].url}'>Sign Here</a>
+            <a href='${responseJson.results[i].url}' target="_blank">Sign Here</a>
             <p>${responseJson.results[i].body}</p>
             `
         )};
@@ -56,6 +81,7 @@ function getPetitions(search){
         $('#js-petitions-form').on('click', '#js-search-button', function(event){
             event.preventDefault();
             const searchVal=$('#js-search-petitions').val();
+            getNews(searchVal);
             getPetitions(searchVal);
         })
     }
